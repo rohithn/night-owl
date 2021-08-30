7eb2631cd2e9e64d6a9886b3b1f856cb89593139
Rohith Nandakumar <rohith.nandakumar@gmail.com>
2021-08-30 19:20:26 +0530

    Fixing CORS

--- server/index.js
+++ server/index.js
@@ -8,11 +8,9 @@ const routes = require("./routes/index");
 // Adding the CORS policy setting
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    "Origin, X-Requested-With, Content-Type, Authorization, Accept"
   );
   next();
 });
