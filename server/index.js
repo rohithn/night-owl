bf6d5049e597768baac7046f33deac0a5e74095c
Rohith Nandakumar <rohith.nandakumar@gmail.com>
2021-08-30 18:22:09 +0530

    Update CORS rule, set heroku server-url for app

--- server/index.js
+++ server/index.js
@@ -8,9 +8,11 @@ const routes = require("./routes/index");
 // Adding the CORS policy setting
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Authorization, Accept"
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
   );
   next();
 });
--- webapp/src/constants/index.js
+++ webapp/src/constants/index.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://night-owl-api.herokuapp.com/";
+// const BASE_URL = "http://localhost:8000";
 export const BOOK_LIST_API_URL = `${BASE_URL}/api/books`;
 export const CATEGORIES_API_URL = `${BASE_URL}/api/categories`;
 export const RATINGS_API_URL = `${BASE_URL}/api/ratings`;
